07311ab5a8212fbc9d877209bbd355bb19592da7
PyBastian <bvivar@DESKTOP-RDNN7FT.localdomain>
2021-11-05 16:25:13 -0300

    aah

--- greeter_client/main.go
+++ greeter_client/main.go
@@ -60,7 +60,7 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 }
 
 func grpcChannel(message string) string {
-	conn, err := grpc.Dial("dist213.inf.santiago.usm.cl:50052", grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.Dial("dist214.inf.santiago.usm.cl:50052", grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
