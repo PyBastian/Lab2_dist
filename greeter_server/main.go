64b5e590e1d6dcc33ed3ac44bb95c1ce677f6443
PyBastian <bvivar@DESKTOP-RDNN7FT.localdomain>
2021-11-05 13:29:20 -0300

    aah

--- greeter_server/main.go
+++ greeter_server/main.go
@@ -58,7 +58,7 @@ func ListenInic() {
 	}
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
-	//log.Printf("server listening at %v", lis.Addr())
+	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
