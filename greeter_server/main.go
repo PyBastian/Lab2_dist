15b6ce64c071b00a3551d38279f00e593aa2feb9
PyBastian <bvivar@DESKTOP-RDNN7FT.localdomain>
2021-12-10 20:01:24 -0300

    primer push Lab2

--- greeter_server/main.go
+++ greeter_server/main.go
@@ -101,7 +101,8 @@ func MsgToNode(msg string) string {
 
 
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-
+	fmt.Printf("Comando Final server \n")
+	fmt.Printf(in.GetName())
 	if in.GetName() == "yes" {
 		N_play = N_play + 1
 		//fmt.Println("Esperando a los Jugadoress, llevamos = ", N_play, " de ", TotalPlayer)
