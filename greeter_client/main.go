00da0598d21ad81c0dee1114ee48c57eb4d4a981
PyBastian <bvivar@DESKTOP-RDNN7FT.localdomain>
2021-12-10 19:11:04 -0300

    primer push Lab2

--- greeter_client/main.go
+++ greeter_client/main.go
@@ -72,7 +72,7 @@ func C_Lider(msg string) string {
 	//fmt.Println("Me voy a comunicar con el Lider")
 	var message string
 
-	message = strings.Split(msg, " ")
+	message = msg
 
 	if message[0] == "AddCity" {
 		//fmt.Println("Entrando al grpcChanel pa mandarle algo al Lider")
@@ -117,7 +117,7 @@ func main() {
 
 	comando := strings.Split(choice, " ")
 	fmt.Println("Hablemos Con el Broker Mos Eisley entonces...")
-	
+
 	for {
 		if comando[0] == "AddCity" {
 			fmt.Println("Okey agregemos")
