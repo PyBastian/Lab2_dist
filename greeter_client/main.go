32141fbb8f173578e3f62f47bb4b705cee29681d
rhernan <rodrigo.hernandezgo@sansano.usm.cl>
2021-12-12 19:00:32 -0300

    update

--- greeter_client/main.go
+++ greeter_client/main.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net"
-	"io"
 	"os"
+	"strings"
+
 	//"strconv"
 	"time"
 	//"strings"
@@ -18,12 +21,13 @@ import (
 const (
 	defaultName = "world"
 )
-func isError(err error) bool {
-    if err != nil {
-        fmt.Println(err.Error())
-    }
 
-    return (err != nil)
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return (err != nil)
 }
 
 var (
@@ -36,6 +40,7 @@ var G_now string = ""
 var id_user string = ""
 var ReadyToPlay string = ""
 var R_Game = ""
+
 func ListenInstr() {
 	lis, err := net.Listen("tcp", "dist216.inf.santiago.usm.cl:50071")
 	if err != nil {
@@ -78,7 +83,7 @@ func grpcChannel(message string) string {
 
 func C_Lider(msg string, n_planeta string, n_ciudad string, n_valor string) string {
 	//fmt.Println("Me voy a comunicar con el Lider")
-	var comando string;
+	var comando string
 
 	comando = msg + " " + n_planeta + " " + n_ciudad + " " + n_valor
 
@@ -113,63 +118,80 @@ func Menu() {
 	fmt.Println("DeleteCity {N_planeta} {N_ciudad} \n")
 
 }
-func createFile(path string){
+func createFile(path string) {
 	// check if file exists
 	var _, err = os.Stat(path)
 	// create file if not exists
 	if os.IsNotExist(err) {
-			var file, err = os.Create(path)
-			if isError(err) {
-					return
-			}
-			defer file.Close()
+		var file, err = os.Create(path)
+		if isError(err) {
+			return
+		}
+		defer file.Close()
 	}
 	fmt.Println("Se ha agregado un nuevo registro", path)
 }
 
-func usecomando(choice string, N_planeta string ,N_ciudad string ,N_valor string){
+func usecomando(choice string, N_planeta string, N_ciudad string, N_valor string) {
 	var path string
 	path = N_planeta + ".txt"
 	fmt.Println(path)
-	if choice == "AddCity"{
+
+	if choice == "AddCity" {
 		createFile(path)
 		var file, err = os.OpenFile(path, os.O_RDWR, 0644)
 		if isError(err) {
-				return
+			return
 		}
 		defer file.Close()
 		_, err = file.WriteString(N_ciudad + " " + N_valor + "\n")
 		if isError(err) {
-				return
+			return
 		}
 		// Read file, line by line
 		var text = make([]byte, 1024)
 		for {
-				_, err = file.Read(text)
-				// Break if finally arrived at end of file
-				if err == io.EOF {
-						break
-				}
-				// Break if error occured
-				if err != nil && err != io.EOF {
-						isError(err)
-						break
-				}
+			_, err = file.Read(text)
+			// Break if finally arrived at end of file
+			if err == io.EOF {
+				break
+			}
+			// Break if error occured
+			if err != nil && err != io.EOF {
+				isError(err)
+				break
+			}
 		}
 		fmt.Println(string(text))
 	}
-	if choice == "UpdateName"{
+
+	if choice == "UpdateName" {
+		input, err := ioutil.ReadFile(path)
+		lines := strings.Split(string(input), "\n")
+		if isError(err) {
+			return
+		}
+		for i, line := range lines {
+			if strings.Contains(line, N_planeta) {
+				lines[i] = "buena"
+			}
+		}
+		output := strings.Join(lines, "\n")
+		err = ioutil.WriteFile(path, []byte(output), 0644)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if choice == "UpdateNumber" {
 
 	}
-	if choice == "UpdateNumber"{
-
-	}
-	if choice == "DeleteCity "{
-    var err = os.Remove(path)
-    if isError(err) {
-        return
-    }
-    fmt.Println("")
+	if choice == "DeleteCity " {
+		var err = os.Remove(path)
+		if isError(err) {
+			return
+		}
+		fmt.Println("")
 	}
 
 }
@@ -193,45 +215,45 @@ func main() {
 		fmt.Println("Hablemos Con el Broker Mos Eisley entonces...")
 		if choice == "AddCity" {
 			fmt.Println("Okey agregemos")
-			respuesta_host = C_Lider(choice, N_planeta,N_ciudad,N_valor)
+			respuesta_host = C_Lider(choice, N_planeta, N_ciudad, N_valor)
 			fmt.Println("El Lider fue Avisado")
 			fmt.Println(respuesta_host)
 			//return
 		}
 		if choice == "UpdateName" {
 			fmt.Println("Okey uName")
-			respuesta_host = C_Lider(choice, N_planeta,N_ciudad,N_valor)
+			respuesta_host = C_Lider(choice, N_planeta, N_ciudad, N_valor)
 			fmt.Println("El Broker fue Avisado, la info se va a la maquina")
 			fmt.Println(respuesta_host)
 			//return
 		}
 		if choice == "UpdateNumber" {
 			fmt.Println("Okey uNumber")
-			respuesta_host = C_Lider(choice, N_planeta,N_ciudad,N_valor)
+			respuesta_host = C_Lider(choice, N_planeta, N_ciudad, N_valor)
 			fmt.Println("El Broker fue Avisado, la info se va a la maquina")
 			fmt.Println(respuesta_host)
 			//return
 		}
 		if choice == "DeleteCity" {
 			fmt.Println("Okey dCity")
-			respuesta_host = C_Lider(choice, N_planeta,N_ciudad,N_valor)
+			respuesta_host = C_Lider(choice, N_planeta, N_ciudad, N_valor)
 			fmt.Println("El Broker fue Avisado, la info se va a la maquina")
 			fmt.Println(respuesta_host)
 			//return
 		}
-		if respuesta_host == "213"{
-			usecomando(choice, N_planeta,N_ciudad,N_valor)
+		if respuesta_host == "213" {
+			usecomando(choice, N_planeta, N_ciudad, N_valor)
 			fmt.Printf("Vamos a guardar la wea en dist 213")
 		}
-		if respuesta_host == "215"{
-			usecomando(choice, N_planeta,N_ciudad,N_valor)
+		if respuesta_host == "215" {
+			usecomando(choice, N_planeta, N_ciudad, N_valor)
 			fmt.Printf("Vamos a guardar la wea en dist 215")
 		}
-		if respuesta_host == "216"{
-			usecomando(choice, N_planeta,N_ciudad,N_valor)
+		if respuesta_host == "216" {
+			usecomando(choice, N_planeta, N_ciudad, N_valor)
 			fmt.Printf("Vamos a proceder a guardar aqui nomas ch en 216")
 		}
 		fmt.Println("Finalizado, puedes ingresar nuevo comando")
-	<-forever
+		<-forever
 	}
 }
