4e92c3fc30cf7fce8221b54de1201f2eca8a2108
rhernan <rodrigo.hernandezgo@sansano.usm.cl>
2021-12-14 15:52:54 -0300

    hdfgfd

--- greeter_client/main.go
+++ greeter_client/main.go
@@ -340,7 +340,7 @@ func main() {
 			usecomando(choice, N_planeta, N_ciudad, N_valor)
 		}
 		fmt.Println("Finalizado, puedes ingresar nuevo comando")
+		respuesta_host = C_Lider(choice, N_planeta, N_ciudad, N_valor)
 		<-forever
-		continue
 	}
 }
